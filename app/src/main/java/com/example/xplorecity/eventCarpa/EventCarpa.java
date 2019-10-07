218f6a085f9e89d775896d64c29b8bb4dc768a6e
alvaro55 <alvaro55cuartero98@gmail.com>
2019-10-07 17:44:07 +0200

    New GUI and first event

---
+++ app/src/main/java/com/example/xplorecity/eventCarpa/EventCarpa.java
@@ -0,0 +1,17 @@
+package com.example.xplorecity.eventCarpa;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.xplorecity.R;
+
+public class EventCarpa extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_carpa);
+    }
+}
--- app/src/main/java/com/example/xplorecity/mainScreen/MainScreenActivity.java
+++ app/src/main/java/com/example/xplorecity/mainScreen/MainScreenActivity.java
@@ -1,16 +1,28 @@
 package com.example.xplorecity.mainScreen;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import com.example.xplorecity.R;
+import com.example.xplorecity.eventCarpa.EventCarpa;
 
-public class MainScreenActivity extends AppCompatActivity {
+public class MainScreenActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        CardView c = findViewById(R.id.event1);
+        c.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent i = new Intent(this, EventCarpa.class);
+        startActivity(i);
     }
 }
